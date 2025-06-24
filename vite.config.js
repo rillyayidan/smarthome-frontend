ddc2a029e4936e034be4dac697acab88d1114d2d
rillyayidan <zidanrilly09@gmail.com>
2025-06-24 07:06:37 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react()
-  ],
-  define:{
-    'import.meta.env.VITE_API_URL': JSON.stringify(env.VITE_API_URL)
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [tailwindcss(), react()],
+    define: {
+      'import.meta.env.VITE_API_URL': JSON.stringify(env.VITE_API_URL)
+    }
   }
 })
